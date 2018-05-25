be906f79fd9a4298499a92d136b20ad67fcae2e6
overtrue <anzhengchao@gmail.com>
2018-05-25 18:26:21 +0800

    Add validation rules

---
+++ app/Validators/PolyExistsValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Validators;
+
+
+/**
+ * Class PolyExistsValidator
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class PolyExistsValidator
+{
+    public function validate($attribute, $value, $parameters, $validator)
+    {
+        if (!$objectType = array_get($validator->getData(), $parameters[0], false)) {
+            return false;
+        }
+
+        try {
+            return !empty(resolve($objectType)->find($value));
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return false;
+        }
+    }
+}
