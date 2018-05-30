de72ec89400c93471fbedfb1e4e275d4bb7e5887
overtrue <anzhengchao@gmail.com>
2018-05-30 20:43:45 +0800

    Notifications.

--- routes/api.php
+++ routes/api.php
@@ -3,24 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::resources([
     'threads' => 'ThreadController',
     'nodes' => 'NodeController',
     'banners' => 'BannerController',
     'tags' => 'TagController',
     'comments' => 'CommentController',
+    'notifications' => 'NotificationController',
 ]);
+Route::post('notifications/mark-all-as-read', 'NotificationController@markAllAsRead')
+            ->name('notifications.mark_all_as_read');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
---
+++ tests/Feature/NotificationTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Notifications\Welcome;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class NotificationTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUserCanGetNotifications()
+    {
+        $user = \factory(User::class)->create();
+
+        $user->notify(new Welcome());
+
+        // not logged
+        $this->getJson('api/notifications')->assertStatus(401);
+
+        // logged
+        $notifications = json_decode($this->actingAs($user, 'api')->getJson('api/notifications')
+            ->assertStatus(200)
+            ->getContent(), true);
+
+        $this->assertCount(1, $notifications);
+        $this->assertArraySubset(['data' => ['user_id' => $user->id]], $notifications[0]);
+    }
+
+    public function testUserCanMarkNotificationAsRead()
+    {
+        $user = \factory(User::class)->create();
+
+        $user->notify(new Welcome());
+
+        $notifications = json_decode($this->actingAs($user, 'api')->getJson('api/notifications')
+            ->assertStatus(200)
+            ->getContent(), true);
+        $first = $notifications[0];
+
+        $this->patchJson('api/notifications/' . $first['id'])->assertStatus(200);
+        $notifications = json_decode($this->actingAs($user, 'api')->getJson('api/notifications')
+            ->assertStatus(200)
+            ->getContent(), true);
+        $first = $notifications[0];
+
+        $this->assertNotNull($first['read_at']);
+    }
+
+    public function testUserCanMarkAllNotificationAsRead()
+    {
+        $user = \factory(User::class)->create();
+
+        $user->notify(new Welcome());
+        $user->notify(new Welcome());
+
+        $this->actingAs($user, 'api')->postJson('api/notifications/mark-all-as-read')
+                ->assertStatus(200);
+
+        $notifications = json_decode($this->actingAs($user, 'api')->getJson('api/notifications')
+            ->assertStatus(200)
+            ->getContent(), true);
+
+        $this->assertNotNull($notifications[0]['read_at']);
+        $this->assertNotNull($notifications[1]['read_at']);
+    }
+}
