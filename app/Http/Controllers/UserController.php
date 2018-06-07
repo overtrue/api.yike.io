349a39604c3098113320253b4a4ff3ad24ab06a6
jcc <changejian@gmail.com>
2018-06-07 20:41:03 +0800

    Optimized

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api')->except(['index', 'activate']);
+        $this->middleware('auth:api')->except(['index', 'show', 'activate']);
     }
 
     public function index(Request $request)
