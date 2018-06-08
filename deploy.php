a2881f1d2c4854b03d48839b7b9323b6d96e7983
overtrue <anzhengchao@gmail.com>
2018-06-08 18:00:05 +0800

    deployer.

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 namespace Deployer;
 
-require 'recipe/common.php';
+require 'recipe/laravel.php';
 
 // Project name
 set('application', 'yikeio api');
@@ -15,7 +15,8 @@ set('ssh_type', 'native');
 set('ssh_multiplexing', true);
 set('default_timeout', 1600);
 set('deploy_path', '/www/api.yike.io');
-set('writable_use_sudo', false);
+set('writable_use_sudo', true);
+set('clear_use_sudo', true);
 set('http_user', 'www-data');
 set('http_group', 'www-data');
 set('writable_mode', 'chown');
@@ -24,13 +25,13 @@ set('keep_releases', 2);
 
 host('api.yike.io')
     ->stage('production')
-    ->user('ubuntu')
+    ->user('deployer')
     ->port(2200)
     ->multiplexing(true);
 
 desc('Restarting php-fpm.');
 task('php-fpm:restart', function(){
-    run('service php7.1-fpm restart');
+    run('sudo service php7.2-fpm restart');
 });
 
 // [Optional] if deploy fails automatically unlock.
