20615a7a548514340cd5f816b5449855f4794373
overtrue <anzhengchao@gmail.com>
2018-06-08 17:15:41 +0800

    deploy.

---
+++ deploy.php
@@ -0,0 +1,42 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'yikeio api');
+
+// Project repository
+set('repository', 'git@gitee.com:yikeio/api.yike.io.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+set('default_timeout', 1600);
+set('deploy_path', '/www/api.yike.io');
+set('writable_use_sudo', false);
+set('http_user', 'www-data');
+set('http_group', 'www-data');
+set('writable_mode', 'chown');
+set('default_stage', 'production');
+set('keep_releases', 2);
+
+host('api.yike.io')
+    ->stage('production')
+    ->user('ubuntu')
+    ->port(2200)
+    ->multiplexing(true);
+
+desc('Restarting php-fpm.');
+task('php-fpm:restart', function(){
+    run('service php7.1-fpm restart');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
+after('cleanup', 'php-fpm:restart');
+after('deploy:unlock', 'artisan:queue:restart');
