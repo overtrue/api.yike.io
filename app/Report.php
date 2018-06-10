c4dbaea7fa04225677737057c45f691b8a725b59
jcc <changejian@gmail.com>
2018-06-10 20:52:54 +0800

    帖子举报

---
+++ app/Report.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+use EloquentFilter\Filterable;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use Filterable;
+
+    protected $fillable = [
+        'user_id', 'remark'
+    ];
+}
---
+++ database/migrations/2018_06_10_112956_create_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('reportable_id');
+            $table->string('reportable_type');
+            $table->string('remark');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
