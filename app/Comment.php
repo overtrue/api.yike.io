b9a2608db717cfcc92a25648a4f5a0060171ad9f
overtrue <anzhengchao@gmail.com>
2018-06-19 19:17:29 +0800

    修复内容同时被修改的bug

--- app/Comment.php
+++ app/Comment.php
@@ -63,7 +63,7 @@ class Comment extends Model
 
         $saveContent = function($comment){
             if (\request('content')) {
-                $data = array_only(\request('content'), \request('type', 'markdown'));
+                $data = array_only(\request('comment.content'), \request('type', 'markdown'));
                 $comment->content()->updateOrCreate(['contentable_id' => $comment->id], $data);
                 $comment->loadMissing('content');
             }
