6e66978510fb7dbcb48c6369f8126cf81e08550d
overtrue <anzhengchao@gmail.com>
2018-08-09 10:45:04 +0800

    Mail style.

--- resources/views/mails/new-comment.blade.php
+++ resources/views/mails/new-comment.blade.php
@@ -1,11 +1,13 @@
 @component('mail::message')
 
-    [{{ '@' . $comment->user->username }}]({{ $comment->user->url }})  评论了您的文章 [《{{ $comment->commentable->title }}》]({{ $comment->commentable->url .'#comment-'.$comment->id}}) ：
+[{{ '@' . $comment->user->username }}]({{ $comment->user->url }})  评论了您的文章 [《{{ $comment->commentable->title }}》]({{ $comment->commentable->url .'#comment-'.$comment->id}}) ：
 
-    {{ $comment->content->activity_log_content }}
+@component('mail::blockquote')
+ {{ $comment->content->activity_log_content }}
+@endcomponent
 
-    ---
-    Thanks.
 
-    {{ config('app.name') }}
+Thanks.
+
+{{ config('app.name') }}
 @endcomponent
