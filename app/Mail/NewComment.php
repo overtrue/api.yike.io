f9edbf7495aa6c83c473d9086dded5298160e281
overtrue <anzhengchao@gmail.com>
2018-08-09 10:08:07 +0800

    评论邮件

---
+++ app/Mail/NewComment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewComment extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $comment;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param \App\Comment $comment
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mails.new-comment');
+    }
+}
