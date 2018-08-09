6566c834d708b6a297a1d657e52adf896567d26c
overtrue <anzhengchao@gmail.com>
2018-08-09 10:08:07 +0800

    评论邮件

---
+++ resources/views/mails/new-comment.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+
+    [{{ '@' . $comment->user->username }}]({{ $comment->user->url }})  评论了您的文章 [《{{ $comment->commentable->title }}》]({{ $comment->commentable->url .'#comment-'.$comment->id}}) ：
+
+    {{ $comment->content->activity_log_content }}
+
+    ---
+    Thanks.
+
+    {{ config('app.name') }}
+@endcomponent
