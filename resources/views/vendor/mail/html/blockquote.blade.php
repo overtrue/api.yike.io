392928d8d27157bb6c2802343a6372f24c33762c
overtrue <anzhengchao@gmail.com>
2018-08-09 10:45:09 +0800

    Mail style.

---
+++ resources/views/vendor/mail/html/blockquote.blade.php
@@ -0,0 +1,9 @@
+<table class="attributes" width="100%" cellpadding="0" cellspacing="0">
+    <tbody>
+    <tr>
+        <td class="attributes_content">
+            {{ Illuminate\Mail\Markdown::parse($slot) }}
+        </td>
+    </tr>
+    </tbody>
+</table>
