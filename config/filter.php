b3d8f6fa85f857b40ba727789c35d2cbb616d3e7
overtrue <anzhengchao@gmail.com>
2018-08-10 21:19:20 +0800

    添加保留字

--- config/filter.php
+++ config/filter.php
@@ -10,6 +10,9 @@ return [
         'background', 'index', 'deploy', 'package', 'event', 'readme',
         'config', 'oauth', 'material', 'service', 'view', 'authentication',
         'login', 'register', 'node', 'notification', 'filter', 'auth',
-        'destroy',
+        'destroy', 'yike', 'yikeio', 'yikeadmin', 'api', 'user', 'panel',
+        'dashboard', 'explore', 'market', 'marketplace',
+        'logout', 'author', 'weibo', 'md5', 'place', 'store', 'documentation', 'docs',
+        'other', 'install',
     ]
 ];
