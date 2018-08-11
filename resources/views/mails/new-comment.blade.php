014f552b2f17991776774f6c0f3f26e53314d90c
overtrue <anzhengchao@gmail.com>
2018-08-11 16:46:24 +0800

    格式

--- resources/views/mails/new-comment.blade.php
+++ resources/views/mails/new-comment.blade.php
@@ -3,7 +3,7 @@
 [{{ '@' . $comment->user->username }}]({{ $comment->user->url }})  评论了您的文章 [《{{ $comment->commentable->title }}》]({{ $comment->commentable->url .'#comment-'.$comment->id}}) ：
 
 @component('mail::blockquote')
- {{ $comment->content->activity_log_content }}
+ {!! $comment->content->activity_log_content !!}
 @endcomponent
 
 
