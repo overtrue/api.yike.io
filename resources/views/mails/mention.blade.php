7876d64796b96901408165fa4fbbdd68d3e66502
overtrue <anzhengchao@gmail.com>
2018-08-11 16:46:24 +0800

    格式

--- resources/views/mails/mention.blade.php
+++ resources/views/mails/mention.blade.php
@@ -3,7 +3,7 @@
 [{{ '@' . $causer->username }}]({{ $causer->url }})  在 [《{{ $content->contentable->commentable->title }}》]({{ $content->contentable->commentable->url }}) 的评论中提及了您：
 
 @component('mail::blockquote')
-{{ $content->activity_log_content }}
+{!! $content->activity_log_content !!}
 @endcomponent
 
 Thanks.
