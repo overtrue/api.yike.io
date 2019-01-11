79b09d018b6c47f360ccfc438161262dfbedbd5f
overtrue <anzhengchao@gmail.com>
2019-01-11 10:06:55 +0800

    CS.

--- app/Jobs/ThreadAddPopular.php
+++ app/Jobs/ThreadAddPopular.php
@@ -6,8 +6,8 @@ use App\Thread;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 /**
  * Class ThreadAddPopular.
