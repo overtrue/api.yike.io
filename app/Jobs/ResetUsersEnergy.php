c30b72003aff51fa23ce87175b4fee48a486399f
刘豪 <liuhao25@foxmail.com>
2019-03-21 21:18:32 +0800

    fix (#32)
    * [+]文章自动加精
    
    * [*]命名修改
    
    * [*]部分代码优化 
    
    * [+]敏感词过滤
    
    * add .gitignore
    
    * add .gitignore
    
    * Format specification
    
    * 优化
    
    * 代码优化
    
    * 增加用户能量值
    
    * 冲突解决
    
    * 冲突解决
    
    * 文章自动加精问题修复 & 模型生命周期监听优化
    
    * 多余代码删除
    
    * 代码规范修改 & 刷新用户缓存
    
    * fix
    
    * fix

--- app/Jobs/ResetUsersEnergy.php
+++ app/Jobs/ResetUsersEnergy.php
@@ -6,8 +6,8 @@ use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 /**
  * Class ResetUsersEnergy.
