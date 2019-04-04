3495a6f49b2ed4d19c294755f34b60d29626bfd0
刘豪 <liuhao25@foxmail.com>
2019-04-04 16:18:41 +0800

    用户活跃度统计 (#39)
    * [+]文章自动加精
    
    * [*]命名修改
    
    * [*]部分代码优化 
    
    * [+]敏感词过滤
    
    * add .gitignore
    
    * add .gitignore
    
    * Format specification
    
    * 优化
    
    * 代码优化
    
    * 增加用户能量值
    
    * 冲突解决
    
    * 冲突解决
    
    * 文章自动加精问题修复 & 模型生命周期监听优化
    
    * 多余代码删除
    
    * 代码规范修改 & 刷新用户缓存
    
    * fix
    
    * fix
    
    * 添加用户签到统计 Job

---
+++ app/Jobs/SignStatistics.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Cache;
+
+class SignStatistics implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var \App\User
+     */
+    protected $user;
+
+    /**
+     * ActivityStatistics constructor.
+     *
+     * @param \App\User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $redis = app('redis.connection');
+
+        $monthKey = \now()->format('Ym');
+        $dayKey = \now()->format('Ymd');
+
+        if ($redis->getbit('login:' . $dayKey, $this->user->id)) {
+            return;
+        }
+
+        $redis->setbit('login:' . $dayKey, $this->user->id, 1);
+        $redis->zincrby('rank:' . $dayKey, 1, $this->user->id);
+
+        $scans = $redis->scan(0, 'match', 'rank:' . $monthKey . '*');
+
+        $ranKeys = \end($scans);
+
+        \reset($scans);
+
+        while (\current($scans) != 0) {
+            $scans = $redis->scan(\current($scans), 'match', 'rank:' . $monthKey . '*');
+
+            $ranKeys = \array_merge($ranKeys, \end($scans));
+
+            \reset($scans);
+        }
+
+        // 避免重复累加
+        $redis->del('monthRank:' . $monthKey);
+        $redis->zunionstore('monthRank:' . $monthKey, $ranKeys);
+
+        Cache::forever('monthRank:' . $monthKey, $redis->zrevrange('monthRank:' . $monthKey, 0, -1, 'withscores'));
+    }
+}
