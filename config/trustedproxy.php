0d692672a4b29568239e4736b1431efd5b3c19f4
overtrue <anzhengchao@gmail.com>
2019-06-02 15:54:51 +0000

    Apply fixes from StyleCI

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
      * Set trusted proxy IP addresses.
      *
@@ -32,9 +31,9 @@ return [
 
     /*
      * Which headers to use to detect proxy related data (For, Host, Proto, Port)
-     * 
+     *
      * Options include:
-     * 
+     *
      * - Illuminate\Http\Request::HEADER_X_FORWARDED_ALL (use all x-forwarded-* headers to establish trust)
      * - Illuminate\Http\Request::HEADER_FORWARDED (use the FORWARDED header to establish trust)
      * - Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB (If you are using AWS Elastic Load Balancer)
@@ -42,9 +41,8 @@ return [
      * - 'HEADER_X_FORWARDED_ALL' (use all x-forwarded-* headers to establish trust)
      * - 'HEADER_FORWARDED' (use the FORWARDED header to establish trust)
      * - 'HEADER_X_FORWARDED_AWS_ELB' (If you are using AWS Elastic Load Balancer)
-     * 
+     *
      * @link https://symfony.com/doc/current/deployment/proxies.html
      */
     'headers' => Illuminate\Http\Request::HEADER_X_FORWARDED_ALL,
-
 ];
